2a198c1d2d1e9b560d4ef4bbb3895b5da316c2b9
NikhilKhatri1 <nikhilkhattri2@gmail.com>
2024-10-09 02:02:32 +0530

    Updated Landing Page

--- client/src/components/LandingPage.jsx
+++ client/src/components/LandingPage.jsx
@@ -10,16 +10,18 @@ const LandingPage = () => {
           </div>
           <div className='user-login fs-5'>
             <span className='me-3 fw-semibold'><a>Home</a></span>
-            <span className='px-4 py-2 mx-3 rounded-full bg-slate-200 button-slate'><a>Login</a></span>
-            <span className='px-4 py-2 rounded-full bg-slate-200 button-slate'><a>Sign Up</a></span>
+            <button className='px-4 py-2 mx-3 rounded-full bg-slate-200 button-slate'><a>Login</a></button>
+            <button className='px-4 py-2 rounded-full bg-slate-200 button-slate'><a>Sign Up</a></button>
           </div>
 
         </nav>
       </header>
       <section className='h-[800px] main flex justify-center align-items-center text-white p-5'>
-        <div className="border-1 row">
-          <div className="col-6 border-1">
-            <h1 className='fs-1 fw-semibold'>Property Management Hub</h1>
+        <div className="row">
+          <div className="col-6 c-1">
+            <p className='heading'>Property Management Hub</p>
+            <p className='para'>Simplify your operations, increase productivity, and unlock the full potential of your real estate investments with our powerful Property Management Hub</p>
+            <button className='px-4 py-2 rounded-full text-dark button-white'><a>Login</a></button>
           </div>
           <div className=" col-6">
             <img src="bodyimg.png" alt="bodylogo" className='img1' />
