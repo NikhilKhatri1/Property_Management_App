b45927187a4896919bead73419d8b07323a5e3ee
NikhilKhatri1 <nikhilkhattri2@gmail.com>
2024-10-09 17:14:05 +0530

    .

--- client/src/components/LandingPage.jsx
+++ client/src/components/LandingPage.jsx
@@ -1,30 +1,43 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 
 const LandingPage = () => {
+  const [menuIcon, setMenuIcon] = useState(false);
+
+  const ToggleMenu = () => {
+    setMenuIcon((prev) => !prev);
+  }
+
   return (
     <div>
       <header className='top-0 position-sticky'>
-        <nav className='h-[85px] d-flex justify-content-between align-items-center px-14'>
-          <div className='logo'>
-            <span><a href="/"><img src="brandLogo.svg" alt="logo" className='w-[170px]' /></a></span>
+        <nav className='h-[85px] d-flex justify-content-between align-items-center'>
+          <div className='brand-logo'>
+            <a href="/"><img src="brandLogo.svg" alt="logo" className='logoimg' /></a>
           </div>
-          <div className='user-login fs-5'>
-            <span className='me-5 fw-semibold home'><Link to='/'>Home</Link></span>
+          <div className='menu-icon'>
+            <button
+              className={`btn fs-1 me-3 ${menuIcon ? 'rotate-icon' : ''}`}
+              onClick={ToggleMenu}
+              aria-label={menuIcon ? "Close Menu" : "Open Menu"}>
+              <i className={menuIcon ? "bi bi-x-lg" : "bi bi-list"}></i>
+            </button>
+          </div>
+          <div className={`user-login fs-5 transition-menu ${menuIcon ? 'display-block' : 'display-none'}`}>
+            <span className='me-lg-5 mt-lg-2 fw-semibold home'><Link to='/'>Home</Link></span>
             <Link to='/login' className='px-4 py-2 mx-3 rounded-full bg-slate-200 button-slate'>Login</Link>
             <Link to='/signup' className='px-4 py-2 rounded-full bg-slate-200 button-slate'>Sign Up</Link>
           </div>
-
         </nav>
       </header>
       <section className='h-[800px] main flex justify-center align-items-center text-white p-5'>
         <div className="row">
-          <div className="col-6 c-1">
+          <div className="col-lg-6 col-12 c-1">
             <p className='heading'>Property Management Hub</p>
             <p className='para'>Simplify your operations, increase productivity, and unlock the full potential of your real estate investments with our powerful Property Management Hub</p>
             <Link to='/login' className='px-4 py-2 rounded-full text-dark button-white'>Login</Link>
           </div>
-          <div className=" col-6">
+          <div className="col-lg-6 col-12">
             <img src="bodyimg.png" alt="bodylogo" className='img1' />
           </div>
         </div>
@@ -33,4 +46,4 @@ const LandingPage = () => {
   )
 }
 
-export default LandingPage
+export default LandingPage;
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,11 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
+import '../node_modules/bootstrap/dist/js/bootstrap.bundle.js';
 
 // import App from './App.jsx'
-import './index.css'
+// import './index.css'
 // import LandingPage from './components/LandingPage.jsx';
 import App from './App.jsx';
+// import Navbar from './components/Navbar.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
