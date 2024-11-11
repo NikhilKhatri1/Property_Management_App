147527a794a11a600c417be7af6c76fb2718f133
NikhilKhatri1 <nikhilkhattri2@gmail.com>
2024-11-11 23:02:15 +0530

    .

--- server/test-bcrypt.js
+++ server/test-bcrypt.js
@@ -1,6 +1,6 @@
 const bcrypt = require('bcrypt');
 const hash = "$2b$10$bg3iAP6WvWZWXfnRTbsyuODVGD04Vv6nKRrFQ.cNvDn24KaLe0.46"; // From DB
-const password = "Ankit123"; // Plaintext password
+const password = "Ankit124"; // Plaintext password
 
 bcrypt.compare(password, hash, (err, isMatch) => {
     if (err) {
